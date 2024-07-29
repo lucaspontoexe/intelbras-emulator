479d06cb170606ae44613e0e4a9395a0228f844a
lucaspontoexe <ltx16@hotmail.com>
2024-07-29 14:17:41 -0300

    login challenge part 2

--- gen-session.ts
+++ gen-session.ts
@@ -1,3 +1,5 @@
+import { Md5 } from "https://deno.land/std@0.119.0/hash/md5.ts";
+
 function generateSessionChars() {
     const characters = [];
     for (let num = 48; num <= 57; num++) characters.push(num);
@@ -13,7 +15,17 @@ export function generateSessionToken(length = 26) {
     for (let i = 0; i < length; i++) {
         token.push(chars.at(Math.round(Math.random() * chars.length)));
     }
-    return token.join('');
+    return token.join("");
 }
 
-if (import.meta.main) console.log(generateSessionToken());
+/**
+ * make password hash
+ * @param  {...string} input
+ * @returns
+ */
+export function makePWHash(...input: string[]) {
+    const md5 = new Md5();
+    const hex_md5 = (str: string) =>
+        md5.update(str).toString("hex").toUpperCase();
+    return hex_md5(input.join(":"));
+}
--- routes.ts
+++ routes.ts
@@ -1,40 +1,36 @@
 import { Router } from "@oak/oak";
 import { LoginRequest, RPC2Response } from "./rpc2.d.ts";
-import { generateSessionToken } from "./gen-session.ts";
+import { generateSessionToken, makePWHash } from "./gen-session.ts";
 
 export const router = new Router();
 
 router.get("/", (ctx) => ctx.response.body = "tem");
 
 router.post("/RPC2_Login", async (ctx) => {
-    const { params, id }: LoginRequest = await ctx.request.body.json();
+    const { params, id, session }: LoginRequest = await ctx.request.body.json();
 
     // hardcoded pro teste
     const _username = "ademir";
     const _password = "senha";
 
+    const loginChallengeParams = {
+        realm: "Login to INTELBRAS EMULATOR 2000",
+        random: crypto.randomUUID(),
+        encryption: "Default",
+    };
     // LOGIN 1
     if (!params.password) {
         // response params: generate random uuid
         // create session (26 random A-z 0-9 chars)
-        // add session to map, etc
-        // store username to cookies. use state?
-
         // retornar "erro" com o login challenge
 
-        const responseParams = {
-            realm: "Login to INTELBRAS EMULATOR 2000",
-            random: crypto.randomUUID(),
-            encryption: "Default",
-        };
-
-        const response: RPC2Response<typeof responseParams> = {
+        const response: RPC2Response<typeof loginChallengeParams> = {
             result: false,
             error: {
                 "code": 268632079,
                 "message": "Component error: login challenge!",
             },
-            params: responseParams,
+            params: loginChallengeParams,
             id,
             // generate and store session
             session: generateSessionToken(),
@@ -44,12 +40,37 @@ router.post("/RPC2_Login", async (ctx) => {
 
         // LOGIN 2
     } else {
+        const expectedHash = makePWHash(
+            params.userName,
+            loginChallengeParams.random,
+            makePWHash(_username, loginChallengeParams.realm, _password),
+        );
+
+        const isPasswordCorrect = params.password === expectedHash;
+
         // check if hashing is correct
         // o código pra isso tem lá no cliente
+
+        if (!isPasswordCorrect) {
+            // TODO: erro wrong password etc
+            ctx.response.status = 401;
+            ctx.response.body = { "NotifyMethod": "1.0" };
+            return;
+        }
+
         // se estiver certo, retorna um keepAliveInterval: 30
+        // add session to map, etc (ctx.state?)
+        const response: RPC2Response<Record<string, number>> = {
+            result: true,
+            params: { keepAliveInterval: 30 },
+            id,
+            session: session as string,
+        };
+        ctx.response.body = response;
+        return;
     }
 
-    ctx.response.body = "rpc2 login";
+    // ctx.response.body = "rpc2 login";
 });
 
 router.get(
